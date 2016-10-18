ee5d25a7119d48aa1570037b7067a8df92536b61
Fernando Silva <fernando.debrando@gmail.com>
2016-10-18 10:48:59 -0200

    Adicionando conexão com banco de dados

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php
 
-phpinfo();
+$con_string = "host=ec2-54-221-226-148.compute-1.amazonaws.com 
+			   port=5432 
+			   dbname=dbudkrpnc8vlqe 
+			   user=njjppqkrydedey 
+			   password=s2V7huGA_Kqv94hsHin39fhtpV";
+
+pg_connect($con_string) or die('Conexão falhou!');
+
+echo "Conectado no Postgres com sucesso!";
